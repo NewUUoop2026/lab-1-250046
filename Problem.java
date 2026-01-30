08433cc9c3d861cc15a1cbf8801711055bc64d4f
ASAD <250046@newuu.uz>
2026-01-30 09:31:02 +0500

    PROBLEM-1

---
+++ Problem.java
@@ -0,0 +1,6 @@
+void main() {
+    double width = 4.5;
+    double height = 7.9;
+    System.out.println("Area: " + Math.ceil((width * height)));
+    System.out.println("Perimeter: " + (2*(width+height)));
+}
