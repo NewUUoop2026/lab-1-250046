cbc4c9823b26217b8293a1011b63d6c616e747bf
ASAD <250046@newuu.uz>
2026-01-30 09:45:54 +0500

    PROBLEM-2

---
+++ Problem2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+void main() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("============================================");
+    System.out.print("Enter the radius and length of a cylinder: ");
+    double x = sc.nextFloat();
+    double y = sc.nextFloat();
+    double pi = 3.1415926535;
+    double area = x*x*pi;
+    System.out.println("The area is " + Math.round((area)));
+    System.out.println("The volue is " + Math.round((area*y)));
+    System.out.println("============================================");
+}
